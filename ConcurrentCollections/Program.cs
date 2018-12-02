1a254bfe16335d1116668fc87a397e331b4f96c6
Mindaugas Lazauskas <mind.lazauskas@gmail.com>
2018-12-02 14:33:05 +0200

    project files

---
+++ ConcurrentCollections/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConcurrentQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var orders = new ConcurrentQueue<string>();
+
+            Task task1 = Task.Run(() => PlaceOrders(orders, "Mark"));
+
+            Task task2 = Task.Run(() => PlaceOrders(orders, "Susan"));
+
+            Task.WaitAll(task1, task2);
+
+            foreach (var order in orders)
+            {
+                Console.WriteLine(order);
+            }
+        }
+
+        static void PlaceOrders(ConcurrentQueue<string> orders, string name)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Thread.Sleep(1);
+
+                string orderName = $"{name} wants a t-shirt - {i}";
+
+                orders.Enqueue(orderName);
+            }
+        }
+    }
+}
---
+++ ConcurrentDictionary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace ConcurrentDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stock = new ConcurrentDictionary<string, int>();
+
+            bool success;
+
+            success = stock.TryAdd("blue", 1);
+            success = stock.TryAdd("red", 2);
+            success = stock.TryAdd("green", 3);
+            
+            success = stock.TryUpdate("red", 3, 2);
+
+            var updatedValue = stock.AddOrUpdate("red", 1, (key, oldValue) => oldValue + 1);
+
+            var yellowValue = stock.GetOrAdd("yellow", 1);
+
+            if (stock.TryGetValue("red", out int value))
+            {
+                Console.WriteLine($"The value of red: {value}");
+            }
+
+            if (stock.TryRemove("red", out int removedValue))
+            {
+                Console.WriteLine($"Removed red with a value of {removedValue}");
+            }
+
+            foreach (var item in stock)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+    }
+}
